4392de41b09272f9b3d485dcdcdd6574da1428bf
lokeshsm <lsakamoo@nyit.edu>
2021-03-30 18:26:05 -0400

    Spring boot App

---
+++ demo-application/src/main/java/com/vir/demoapplication/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vir.demoapplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
